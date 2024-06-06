771b8d5114eb8cae2bdf49f2e1633b663b751605
Kat Grace <katherine.grace.reed@gmail.com>
2024-06-06 18:14:22 +0100

    branding
    Signed-off-by: Kat Grace <katherine.grace.reed@gmail.com>

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,9 @@
 export default function Header() {
     return (
         <header className="title">
+            <img src="/Reed-in-Partnership-Logo.png" alt="Reed in Partnership Logo" className="rinp-logo"/>
             <h1>LGBTQ+ Steering Group</h1>
+            <img src="/Reed-in-Partnership-Logo.png" alt="Reed in Partnership Logo" className="rinp-logo" id="desktop"/>
         </header>
     )
 }
