80f44fb850581dd79a8180f087ef1f06bbad7a64
Kat Reed <142042675+Katherine-Reed@users.noreply.github.com>
2025-05-04 17:50:10 +0100

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,9 @@
 export default function Header() {
     return (
         <header className="title">
-            <img src="/Reed-in-Partnership-Logo.png" alt="Reed in Partnership Logo" className="rinp-logo"/>
+            <img src="" alt="Logo" className="rinp-logo"/>
             <h1>LGBTQ+ Steering Group</h1>
-            <img src="/Reed-in-Partnership-Logo.png" alt="Reed in Partnership Logo" className="rinp-logo" id="desktop"/>
+            <img src="" alt="Logo" className="rinp-logo" id="desktop"/>
         </header>
     )
 }
